dcd6dd2a7fe5b18a1b3826e0e60419425cf2b0fd
Vladimir <28butterfly10@gmail.com>
2024-06-02 20:13:37 +0300

    Style | add style for tires-filter

--- src/pages/filterTires/FilterTires.tsx
+++ src/pages/filterTires/FilterTires.tsx
@@ -1,13 +1,28 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './FilterTires.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import '../filterTires/FilterTires.css';
+import { tires, ITires } from './tires';
 
 function FilterTires() {
   const [width, setWidth] = useState('205');
   const [profile, setProfile] = useState('55');
   const [diametr, setDiametr] = useState('16');
+  const [checkManufacturer, setCheckManufacturer] = useState<ITires>(tires);
+
+  const [openManufacturer, setOpenManufacturer] = useState(false);
+  const [openSeason, setOpenSeason] = useState(false);
+
   const navigate = useNavigate();
+
+  function handleOpenManufacturer() {
+    setOpenManufacturer(!openManufacturer);
+  }
+
+  function handleOpenSeason() {
+    setOpenSeason(!openSeason);
+  }
 
   function handleChangeWidth(event: React.ChangeEvent<HTMLSelectElement>) {
     setWidth(event.target.value);
@@ -21,8 +36,161 @@ function FilterTires() {
     setDiametr(event.target.value);
   }
 
+  // function updateCheckStatus(index) {
+  //   checkManufacturer.premium.map((item, currentIndex) => {
+  //     if (index == currentIndex) {
+  //       item.checked = !item.checked;
+  //     }
+  //   });
+  //   setCheckManufacturer(checkManufacturer);
+  // }
+
+  // function selectAll() {
+  //   checkManufacturer.premium.map(item => {
+  //     item.checked = !item.checked;
+  //   });
+  //   console.log(checkManufacturer.premium);
+  //   setCheckManufacturer(checkManufacturer);
+  // }
+  // function unSelectAll() {
+  //   setCheckManufacturer(checkManufacturer.map(item => ({ ...item, checked: false })));
+  // }
+
   function handleClickSearch() {
     navigate(`/filter/${width} /${profile} /${diametr}`);
+  }
+
+  function dropdownManufacturer() {
+    return (
+      <div className="choose-manufacturer">
+        <label className="title">Producent / Klasa</label>
+        <div className="dropdown" onClick={handleOpenManufacturer}>
+          <span>Wszyscy</span>
+          <FontAwesomeIcon icon={faChevronDown} />
+        </div>
+        {openManufacturer ? (
+          <>
+            <div className="producers-group">
+              <div className="group-premium">
+                <div className="group-premium__title">
+                  <input type="checkbox" name="allPremium" id="allPremium" />
+                  <label htmlFor="allPremium">Klasa premium</label>
+                </div>
+                <div className="group-premium__items">
+                  <ul>
+                    {tires.premium.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <label htmlFor={item.name}>
+                            <input type="checkbox" name={item.name} id={item.name} checked={item.checked} />
+                            <span>{item.name}</span>
+                          </label>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+              <div className="group-medium">
+                <div className="group-medium__title">
+                  <input type="checkbox" id="allMedium" name="allMedium" />
+                  <label htmlFor="allMedium">Klasa średnia</label>
+                </div>
+                <div className="group-medium__items">
+                  <ul>
+                    {tires.medium.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <label htmlFor={item.name}>
+                            <input type="checkbox" name={item.name} id={item.name} />
+                            <span>{item.name}</span>
+                          </label>
+                        </li>
+                      );
+                    })}
+                    {/*  <li className="withoutHover">
+                      <span className="hideRest">
+                        <strong>Więcej</strong> <em>(3)</em>
+                      </span>
+                    </li> */}
+                  </ul>
+                </div>
+              </div>
+              <div className="group-cheap">
+                <div className="group-cheap__title">
+                  <input type="checkbox" id="allCheap" name="allCheap" />
+                  <label htmlFor="allCheap">Klasa ekonomiczna</label>
+                </div>
+                <div className="group-cheap__items">
+                  <ul>
+                    {tires.cheap.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <label htmlFor={item.name}>
+                            <input type="checkbox" name={item.name} id={item.name} />
+                            <span>{item.name}</span>
+                          </label>
+                        </li>
+                      );
+                    })}
+                    {/* <li className="withoutHover">
+                      <span className="hideRest">
+                        <strong>Więcej</strong> <em>(85)</em>
+                      </span>
+                    </li> */}
+                  </ul>
+                </div>
+              </div>
+              <div className="choose-manufacturer__btn">
+                <button>Zastosuj</button>
+              </div>
+            </div>
+          </>
+        ) : null}
+      </div>
+    );
+  }
+
+  function dropdownSeason() {
+    return (
+      <div className="choose-season">
+        <label className="title" htmlFor="season">
+          Sezon
+        </label>
+        <div className="dropdown" onClick={handleOpenSeason}>
+          <span>Wszystkie</span>
+          <FontAwesomeIcon icon={faChevronDown} />
+        </div>
+        {openSeason ? (
+          <div className="dropdown__season">
+            <ul className="dropdown__season__menu">
+              <li>
+                <label title="Letnie" htmlFor="Letnie">
+                  <input id="Letnie" type="checkbox" name="Letnie" />
+                  <p>Letnie </p>
+                </label>
+              </li>
+              <li>
+                <label title="Zimowe" htmlFor="Zimowe">
+                  <input id="Zimowe" type="checkbox" name="Zimowe" />
+                  <p>Zimowe </p>
+                </label>
+              </li>
+              <li>
+                <label title="Całoroczne" htmlFor="Całoroczne">
+                  <input id="Całoroczne" type="checkbox" name="Całoroczne" />
+                  <p>Całoroczne </p>
+                </label>
+              </li>
+            </ul>
+            <hr />
+            <div className="dropdown__season__btn">
+              <button>Zastosuj</button>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
   }
 
   return (
@@ -30,25 +198,24 @@ function FilterTires() {
       <div className="header-selector">
         <ul>
           <li>
-            Поиск шин <strong>по размеру</strong>
+            Szukaj opon <strong>wg rozmiaru</strong>
           </li>
         </ul>
       </div>
       <div className="wrapper">
         <div className="product-type">
-          <p>Автомобильные шины</p>
-          <picture>
-            <source media="(max-width: 959px)" srcSet="/gfx/common/blank.gif" type="image/gif"></source>
-            <source media="(min-width: 960px)" srcSet="/gfx/common/big-car-tire-summer.webp" type="image/webp"></source>
-            <source media="(min-width: 960px)" srcSet="/gfx/common/big-car-tire-summer.png" type="image/png"></source>
-            <img src="https://www.oponeo.pl/gfx/common/big-car-tire-summer.webp" width="211" height="472" alt="Автомобильные шины"></img>
-          </picture>
+          <p>
+            Opony <strong>samochodowe</strong>
+          </p>
+          <div className="wrapper-image"></div>
         </div>
         <div className="tab-content">
           <div className="choose-width">
-            <label htmlFor="width">Ширина</label>
+            <label className="title" htmlFor="width">
+              Szerokość
+            </label>
             <select name="_carTires_ctTS_ddlDimWidth" id="width" defaultValue="205" onChange={handleChangeWidth}>
-              <option value="__NULL__">Выбирать</option>
+              <option value="__NULL__">Wybierz</option>
               <option value="1.40">1.40</option>
               <option value="4.00">4.00</option>
               <option value="5.00">5.00</option>
@@ -60,7 +227,7 @@ function FilterTires() {
               <option value="27">27</option>
               <option value="28">28</option>
               <option value="29">29</option>
-              <option value="30">тридцать</option>
+              <option value="30">30</option>
               <option value="31">31</option>
               <option value="32">32</option>
               <option value="33">33</option>
@@ -96,9 +263,9 @@ function FilterTires() {
             </select>
           </div>
           <div className="choose-profile">
-            <label>Профиль</label>
+            <label className="title">Profile</label>
             <select name="_carTires_ctTS_ddlDimRatio" defaultValue="55" onChange={handleChangeProfile}>
-              <option value="__NULL__">Выбирать</option>
+              <option value="__NULL__">Wybierz</option>
               <option value="35">35</option>
               <option value="40">40</option>
               <option value="45">45</option>
@@ -109,23 +276,25 @@ function FilterTires() {
               <option value="70">70</option>
               <option value="75">75</option>
               <option value="80">80</option>
-              <option value="__OTHER__">другой</option>
+              <option value="__OTHER__">inny</option>
             </select>
           </div>
           <div className="choose-diametr">
-            <label>Диаметр</label>
+            <label className="title">Średnica</label>
             <select name="_carTires_ctTS_ddlDimDiameter" defaultValue="16" onChange={handleChangeDiametr}>
-              <option value="__NULL__">Выбирать</option>
+              <option value="__NULL__">Wybierz</option>
               <option value="15">15</option>
               <option value="16">16</option>
               <option value="17">17</option>
               <option value="18">18</option>
               <option value="19">19</option>
-              <option value="__OTHER__">другой</option>
+              <option value="__OTHER__">inny</option>
             </select>
           </div>
+          {dropdownManufacturer()}
+          {dropdownSeason()}
           <div className="btn-wrapper">
-            <button onClick={handleClickSearch}>Поиск шин</button>
+            <button onClick={handleClickSearch}>Szukaj opon</button>
           </div>
         </div>
       </div>
