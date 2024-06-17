491a461505f0fef65c9bd57f4b49c81a57a62792
ArtMiProg <am.poshta1@gmail.com>
2024-06-17 19:08:25 +0300

    |Fix | redundant variables

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD-HnqmSng6YZi-hd6QPp403WMGWFOdLE0",
-//   authDomain: "tires-dm.firebaseapp.com",
-//   projectId: "tires-dm",
-//   storageBucket: "tires-dm.appspot.com",
-//   messagingSenderId: "48235976284",
-//   appId: "1:48235976284:web:cfdc8ae09739e1d62b3899",
-//   measurementId: "G-K0TJSTVLKM"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyD-HnqmSng6YZi-hd6QPp403WMGWFOdLE0",
+  authDomain: "tires-dm.firebaseapp.com",
+  projectId: "tires-dm",
+  storageBucket: "tires-dm.appspot.com",
+  messagingSenderId: "48235976284",
+  appId: "1:48235976284:web:cfdc8ae09739e1d62b3899",
+  measurementId: "G-K0TJSTVLKM"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
